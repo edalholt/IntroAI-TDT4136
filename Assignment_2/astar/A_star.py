076dbee8386adc834dfadfe6a1e4b008dcc1cb7b
Jakob Keller <jekeller@stud.ntnu.no>
2022-09-29 14:59:35 +0200

    added main function. Also we sort_activeList before finding new_node

--- Assignment_2/astar/A_star.py
+++ Assignment_2/astar/A_star.py
@@ -1,7 +1,4 @@
 from Map import Map_Obj
-
-map_obj = Map_Obj(task=5)
-
 class Node():
     def __init__(self, pos, parent):
         self.pos = pos
@@ -20,6 +17,7 @@ class Node():
             self.cost = map_obj.get_cell_value(self.pos) + self.parent.cost
         self.total = self.cost + self.heur
         
+        
 
     def heuristic(self, goal):
         # absolute value of current x minus goal x, plus current y minus goal y
@@ -37,7 +35,7 @@ class A_star():
         x = self.current.pos[0]
         y = self.current.pos[1]
         self.inherit = []
-        
+        #Hardcoded to 4 because we know each node only has 4 adjacent nodes
         for i in range(4):
             if i == 0:
                 if (map_obj.get_cell_value([x+1, y]) > 0):
@@ -52,7 +50,6 @@ class A_star():
                 if (map_obj.get_cell_value([x, y-1]) > 0):
                     self.inherit.append([x, y-1])
             i += 1
-            
         self.activeList.remove(self.current)
 
     #sorts the activeList based on the total value of a node           
@@ -62,15 +59,14 @@ class A_star():
     #Checks if the inheritent node is in activeList, if it is, we check if the total cost of the new instance is less than the one that is already there.
     #If the cost is less, we replace the old instance with the new one because we have found a shorter path to the node.
     def append_to_activeList_and_check_for_duplicates(self, current_node):
-        for a in range(len(self.activeList)):
-            if current_node.pos == self.activeList[a].pos:
-                if current_node.total < self.activeList[a].total: 
-                    self.activeList.pop(a)
+        for i in range(len(self.activeList)):
+            if current_node.pos == self.activeList[i].pos:
+                if current_node.total < self.activeList[i].total: 
+                    self.activeList.pop(i)
                     self.activeList.append(current_node)
                 return 
-            a += 1 
+            i += 1 
         self.activeList.append(current_node)  
-        self.sort_activeList()
 
     #Loops through all the inherits of the current node and use the function append_to_***
     def next_node(self, parent):
@@ -81,32 +77,33 @@ class A_star():
     #This function acts like a main funciton. Every function is combined to make the A* algorithm work.
     #It adds color to the map showing the shortest path, and every visited node.
     def move_to_new_node(self):
-        steps = 0
-        
         while self.current.pos != map_obj.get_goal_pos():
-            
             self.find_inherit()
-            self.next_node(self.current)
             self.sort_activeList()
+            self.next_node(self.current)
+            
             self.current = self.activeList[0]
-            map_obj.replace_map_values(self.current.pos, 4, map_obj.get_goal_pos())
-
-            #Moves the goal pos in task 5
-            map_obj.tick()
-
-        #Adding a different color to the shortest path
+            #map_obj.replace_map_values(self.current.pos, 4, map_obj.get_end_goal_pos())
+               
+    def draw_shortest_path(self):
+        steps = 0
         while self.current.pos != map_obj.get_start_pos():
             map_obj.replace_map_values(self.current.pos, 6, map_obj.get_start_pos())
             self.current = self.current.parent
             steps+=1
-
-        #Adding a third color to the end position so we can clearly see where it is.
         map_obj.replace_map_values(map_obj.get_goal_pos(), 5, map_obj.get_start_pos())
         print(steps, "steps taken")
-    
-map_obj1 = A_star(map_obj)  
-map_obj1.move_to_new_node()
-map_obj.get_maps()
-map_obj.show_map()
+
+if (__name__ == "__main__"):
+    #Task 5 is not done
+    map_obj = Map_Obj(task=4)
+    map_obj1 = A_star(map_obj)  
+    map_obj1.move_to_new_node()
+    map_obj1.draw_shortest_path()
+    map_obj.get_maps()
+    map_obj.show_map()
+
+    #We edited one line of code in Map.py to set the color of the shortest path to yellow (line 232 - 233)
+                        
 
 
